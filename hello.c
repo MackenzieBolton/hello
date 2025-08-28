9cca6931ae561741bebe2e087dec65f732763433
Mackenzie Bolton <mbolton4@huskers.unl.edu>
2025-08-28 09:46:32 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Mackenzie Bolton
+ * Date: 2025/08/28
+ *
+ * Prints name
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hi Mackenzie\n");
+
+}
